c8c9d5f32c401347d124bf17ca2ac8690f4749cc
miggynascimento <miguel.nascimento@hotmail.ca>
2020-06-24 20:09:07 -0400

    Updates

---
+++ precache-manifest.49046146203c5169f964686c9ba18998.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f9af2abdb2aa357fb9b9f99b6838e3",
+    "url": "/stonks/index.html"
+  },
+  {
+    "revision": "c0cf08b2d0530870196c",
+    "url": "/stonks/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f6ff3e78b878f8d2527",
+    "url": "/stonks/static/js/2.4076d15c.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/stonks/static/js/2.4076d15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0cf08b2d0530870196c",
+    "url": "/stonks/static/js/main.e6972396.chunk.js"
+  },
+  {
+    "revision": "4e5af9afce30f61bb1b7",
+    "url": "/stonks/static/js/runtime-main.d2f40d1a.js"
+  },
+  {
+    "revision": "471d172eb03fede7e0175ea536ffa643",
+    "url": "/stonks/static/media/stonks.471d172e.png"
+  }
+]);
